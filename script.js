095ad9cd293f9a72ede2c9147fda52946824de0f
hakeemgardner <hakeemgardner34@gmail.com>
2023-06-01 09:27:43 -0500

    Minor fixes

--- script.js
+++ script.js
@@ -3,10 +3,21 @@ const dice = document.querySelector(".dice");
 dice.addEventListener("click", fetchData);
 
 async function fetchData() {
-  let res = await fetch("https://api.adviceslip.com/advice");
-  let data = await res.json();
-  const adviceNumber = document.getElementById("number");
-  const advice = document.getElementById("advice");
-  advice.innerHTML = data.slip.advice;
-  adviceNumber.innerHTML = data.slip.id;
+  try {
+    let res = await fetch("https://api.adviceslip.com/advice");
+    if (!res.ok) {
+      throw new Error("Network response was not ok.");
+    }
+    let data = await res.json();
+    const adviceNumber = document.getElementById("number");
+    const advice = document.getElementById("advice");
+    advice.innerHTML = data.slip.advice;
+    adviceNumber.innerHTML = data.slip.id;
+  } catch (error) {
+    // Handle the error
+    console.error("Error:", error);
+    // Display an error message to the user
+    const advice = document.getElementById("advice");
+    advice.innerHTML = "Error occurred while fetching data.";
+  }
 }
